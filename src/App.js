e3db0b764ad52c621912e36f86f1b3307b92016b
Nikolaienko Ihor <nikolasha8876@gmail.com>
2021-04-11 15:22:24 +0300

    lazy

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Switch, Route } from "react-router-dom";
-
 import "./App.css";
-import HomePage from "./components/HomePage/HomePage";
-import MoviesPage from "./components/MoviesPage/MoviesPage";
-import MovieDetailsPage from "./components/MovieDetailsPage/MovieDetailsPage";
+
 import AppBar from "./components/AppBar/AppBar";
 import routes from "./routes";
+
+const HomePage = lazy(() => import("./components/HomePage/HomePage.js"));
+const MoviesPage = lazy(() => import("./components/MoviesPage/MoviesPage.js"));
+const MovieDetailsPage = lazy(() =>
+  import("./components/MovieDetailsPage/MovieDetailsPage.js")
+);
 
 const App = () => (
   <>
     <AppBar />
-    <Switch>
-      <Route exact path={routes.homePage} component={HomePage} />
-      <Route exact path={routes.moviesPage} component={MoviesPage} />
-      <Route path={routes.movieDetailsPage} component={MovieDetailsPage} />
-    </Switch>
+    <Suspense fallback={<h1>.......</h1>}>
+      <Switch>
+        <Route exact path={routes.homePage} component={HomePage} />
+        <Route exact path={routes.moviesPage} component={MoviesPage} />
+        <Route path={routes.movieDetailsPage} component={MovieDetailsPage} />
+      </Switch>
+    </Suspense>
   </>
 );
 
